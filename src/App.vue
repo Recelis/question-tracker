57139740fb93e1147de692425a1e9f5bf1ea9762
Jacky Lui <jackymlui@live.com>
2022-05-12 18:12:53 +1000

    setting up firebase

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppScreen />
 </template>
 
 <script>
+// https://www.smashingmagazine.com/2020/05/vue-survey-app-firebase-authentication-database/
 import HelloWorld from './components/HelloWorld.vue'
+import AppScreen from "./components/AppScreen.vue"
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    AppScreen
+  },
+  mounted: () => {},
+  computed: {
+    dog: "hello"
+  },
 }
 </script>
 
---
+++ src/components/AppScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hello I'm your screen</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'AppScreen',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
