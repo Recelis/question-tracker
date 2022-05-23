687e49f1ff55849e8c9a47a0d8d879183a82f9b7
Jacky Lui <jackymlui@live.com>
2022-05-23 14:30:54 +1000

    setting up firebase (fun)

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,58 @@
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
   <AppScreen />
+  <template v-for="city in cityList" v-bind:key="city._id">
+    <div>{city.name}</div>
+  </template>
 </template>
 
 <script>
 // https://www.smashingmagazine.com/2020/05/vue-survey-app-firebase-authentication-database/
-import HelloWorld from './components/HelloWorld.vue'
-import AppScreen from "./components/AppScreen.vue"
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebase security rules
+import HelloWorld from "./components/HelloWorld.vue";
+import AppScreen from "./components/AppScreen.vue";
+import firebase from "./firebaseInit.js";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+const db = getFirestore(firebase);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    AppScreen
+    AppScreen,
   },
-  mounted: () => {},
+  mounted: async function () {
+    console.log(process.env);
+    let cities = await this.getCities();
+    console.log(cities);
+    // await this.createCity("Hobart");
+  },
   computed: {
-    dog: "hello"
+    dog: "hello",
   },
-}
+  methods: {
+    createCity: async (name) => {
+      try {
+        console.log("hello creating city");
+        const docRef = await addDoc(collection(db, "cities"), { name });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (error) {
+        console.error("Error adding document: ", error);
+      }
+    },
+    getCities: async () => {
+      console.log("getting cities");
+      const citiesCol = collection(db, "cities");
+      const citySnapshot = await getDocs(citiesCol);
+      const cityList = citySnapshot.docs.map((doc) => doc.data());
+      console.log(cityList);
+      return cityList;
+    },
+  },
+};
 </script>
 
 <style>
